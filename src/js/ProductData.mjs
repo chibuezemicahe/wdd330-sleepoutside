d5ef82da0bddbdeacc620e59217495ba0aaf17ff
chibuezemicahe <chibuezemicahe@gmail.com>
2025-09-20 23:46:06 +0100

    fix addto cart

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,15 +9,25 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
+    // Use absolute path instead of relative path for better compatibility with deployment
+    this.path = `/json/${this.category}.json`;
   }
   getData() {
     return fetch(this.path)
       .then(convertToJson)
-      .then((data) => data);
+      .then((data) => data)
+      .catch(err => {
+        console.error("Error fetching product data:", err);
+        return [];
+      });
   }
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    try {
+      const products = await this.getData();
+      return products.find((item) => item.Id === id);
+    } catch (error) {
+      console.error("Error finding product by ID:", error);
+      return null;
+    }
   }
 }
